21ea6fa9a8796cda34e4b80c75759122fc26a8ac
Kaveh <kavehkhorshidiii@gmail.com>
2026-08-08 19:12:05 +0330

    commit

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,4 +1,6 @@
-fetch('partials/header.html')
+// fetch('partials/header.html')
+//fetch('partials/header/header.html')
+fetch('partials/header/header.html')
    .then(res => res.text())
    .then(html => {
       document.getElementById('header-placeholder').innerHTML = html;
