2ae4ab95cb18b8cd8975230bd3bddc97aafeac48
Kaveh <kavehkhorshidiii@gmail.com>
2026-08-09 22:33:07 +0330

    add image

--- frontend/js/slider.js
+++ frontend/js/slider.js
@@ -2,7 +2,7 @@ const slider = document.getElementById('mySlider');
 const nextBtn = document.getElementById('nextBtn');
 const prevBtn = document.getElementById('prevBtn');
 
-const scrollAmount = 220;
+const scrollAmount = 2;
 
 nextBtn.addEventListener('click', () => {
   slider.scrollBy({ left: scrollAmount, behavior: 'smooth' });
